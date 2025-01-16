97d0c96a9bb8be4d479fc88d853e50da1f597063
jyooDev <gina07099@gmail.com>
2025-01-15 23:59:31 -0600

    CRUD for users

---
+++ ReslifeWorkorderManagement/ReslifeWorkorderManagement/Models/UserCreateVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReslifeWorkorderManagement.Models
+{
+    public class UserCreateVM
+    {
+
+        [Required(ErrorMessage = "First name is required.")]
+        public required string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public required string? LastName { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        public required string? Email { get; set; } 
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public required string? Password { get; set; }
+
+        public required string Role { get; set; }
+    }
+}
