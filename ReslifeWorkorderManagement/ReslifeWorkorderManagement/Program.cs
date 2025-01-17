f9c449ae0fd22c5f66e3913f1b4e14473664aad3
jyooDev <gina07099@gmail.com>
2025-01-17 17:02:56 -0600

    quick update to .gitignore

--- ReslifeWorkorderManagement/ReslifeWorkorderManagement/Program.cs
+++ ReslifeWorkorderManagement/ReslifeWorkorderManagement/Program.cs
@@ -56,7 +56,11 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
